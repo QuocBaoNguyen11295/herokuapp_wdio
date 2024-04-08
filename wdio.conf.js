6d3a3fb6ab9f17c78bd3399112bc4df528f23b73
QuocBaoNguyen11295 <nqb11295@gmail.com>
2024-04-08 22:31:44 +0700

    add github actions

--- wdio.conf.js
+++ wdio.conf.js
@@ -4,6 +4,8 @@ const fs = require('fs');
 const pathToChromeDownloads = './chromeDownloads';
 const assert = require('assert');
 const pathToDownload = path.resolve('chromeDownloads');
+const { TimelineService } = require('wdio-timeline-reporter/timeline-service');
+
 exports.config = {
     //
     // ====================
@@ -112,7 +114,7 @@ exports.config = {
     // Services take over a specific job you don't want to take care of. They enhance
     // your test setup with almost no effort. Unlike plugins, they don't add new
     // commands. Instead, they hook themselves up into the test process.
-    // services: [],
+    services: [[TimelineService]],
     //
     // Framework you want to run your specs with.
     // The following are supported: Mocha, Jasmine, and Cucumber
@@ -135,7 +137,7 @@ exports.config = {
     // Test reporter for stdout.
     // The only one supported by default is 'dot'
     // see also: https://webdriver.io/docs/dot-reporter
-    reporters: ['spec'],
+    reporters: [['timeline', { outputDir: './report' }]],
 
     // Options to be passed to Mocha.
     // See the full list at http://mochajs.org/
