cf66963ad09026100dbd90ae0864f3252ffc9a38
QuocBaoNguyen11295 <nqb11295@gmail.com>
2024-04-08 22:37:16 +0700

    update

--- wdio.conf.js
+++ wdio.conf.js
@@ -114,7 +114,7 @@ exports.config = {
     // Services take over a specific job you don't want to take care of. They enhance
     // your test setup with almost no effort. Unlike plugins, they don't add new
     // commands. Instead, they hook themselves up into the test process.
-    services: [[TimelineService]],
+    services: [[TimelineService, { drivers:{chrome:"123.0.6312.105"}}]],
     //
     // Framework you want to run your specs with.
     // The following are supported: Mocha, Jasmine, and Cucumber
